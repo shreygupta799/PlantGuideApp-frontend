bbe6596dd3894121135e40480f5acbece52683cb
shreygupta799 <shreygupta799@gmail.com>
2024-07-24 22:26:48 +0530

    made changes in color

--- lib/res/colors.dart
+++ lib/res/colors.dart
@@ -4,4 +4,5 @@ class AppColor {
   static const Color blackColor = Color(0xFF000000);
   static const Color blueColor = Color.fromARGB(255, 55, 63, 180);
   static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greenColor = Color(0xFF096E0E);
 }
